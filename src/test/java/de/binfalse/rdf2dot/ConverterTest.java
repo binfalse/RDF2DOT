dabda2c5a5df60a770d343822e1bef8c6fd432b3
Martin Scharm <martin@binfalse.de>
2015-10-17 10:09:25 +0200

    tests simple prov

--- src/test/java/de/binfalse/rdf2dot/ConverterTest.java
+++ src/test/java/de/binfalse/rdf2dot/ConverterTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -19,7 +21,7 @@ import de.binfalse.rdf2dot.RdfDotConverter;
 @RunWith(JUnit4.class)
 public class ConverterTest
 {
-	
+	Pattern dotConnection = Pattern.compile (" -> ");
 	
 	
 	/**
@@ -34,8 +36,10 @@ public class ConverterTest
 			tmp.delete ();
 			new RdfDotConverter ().convert ("test/simplegraph.ttl", tmp.getAbsolutePath (), "BT");
 			String contents = GeneralTools.fileToString (tmp);
+			
 			assertTrue ("expected to obtain a BT graph", contents.contains ("rankdir=\"BT\""));
-			assertEquals ("expected to receive a single connection", 2, contents.split (" -> ").length);
+			assertEquals ("expected to receive a single connection", 1, findPattern (dotConnection, contents));
+			assertEquals ("expected to get a node labeled 'a'", 1, findPattern (Pattern.compile ("\"a\" \\[ [^\\]]*label=\"a\"[^\\]]* \\]"), contents));
 			tmp.delete ();
 		}
 		catch (IOException e)
@@ -44,6 +48,16 @@ public class ConverterTest
 			fail ("wasn't able to convert simple graph");
 		}
 	}
+	
+	private int findPattern (Pattern p, String s)
+	{
+		int n = 0;
+		Matcher m = p.matcher (s);
+		while (m.find ())
+			n++;
+		return n;
+	}
+	
 	
 	
 	
@@ -57,14 +71,20 @@ public class ConverterTest
 		{
 			File tmp = File.createTempFile ("Rdf2Dot-test-", "dot");
 			tmp.delete ();
-			new RdfDotConverter ().convert ("test/simplegraph-2.ttl", tmp.getAbsolutePath (), "LR");
+			new RdfDotConverter ().convert ("test/simple-prov.ttl", tmp.getAbsolutePath (), "LR");
 			String contents = GeneralTools.fileToString (tmp);
-			System.out.println (contents);
+			
 			assertTrue ("expected to obtain a LR graph", contents.contains ("rankdir=\"LR\""));
-			assertEquals ("expected to receive a single connection and 2 attribute connections", 4, contents.split (" -> ").length);
+			assertEquals ("expected to receive two ordinary connections and three attribute connections", 5, findPattern (dotConnection, contents));
+			
 			assertTrue ("expected to obtain a prov:associatedWith connection", contents.contains (" [ label=\"prov:associatedWith\" ]"));
+			assertTrue ("expected to obtain a prov:used connection", contents.contains (" [ label=\"prov:used\" ]"));
 			assertTrue ("expected to obtain a house-agent", contents.contains (" [ shape=\"house\" "));
-			assertTrue ("expected to obtain a prov entity", contents.contains (" style=\"filled\"  fillcolor=\"#FFFC87\" ]"));
+			assertTrue ("expected to obtain a prov entity shape", contents.contains (" [ shape=\"house\" "));
+			assertTrue ("expected to obtain prov entity attributes", contents.contains (" style=\"filled\"  fillcolor=\"#FFFC87\" ]"));
+			assertTrue ("expected to obtain prov activity shape", contents.contains ("[ shape=\"house\" "));
+			assertTrue ("expected to obtain prov activity attributes", contents.contains (" style=\"filled\"  fillcolor=\"#FDB266\" ]"));
+			
 			assertFalse ("didn't expect a file name if speaking about internal stuff", contents.contains (new File ("test/simplegraph-2.ttl").getAbsoluteFile ().getParentFile ().getParentFile ().getAbsolutePath ()));
 			assertFalse ("didn't expect a file name if speaking about internal stuff", contents.contains ("simplegraph"));
 			assertFalse ("didn't expect a file name if speaking about internal stuff", contents.contains (tmp.getName ()));
