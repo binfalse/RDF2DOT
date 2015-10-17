72f9a1479bf4e7f5920d9724387201c117c38d79
Martin Scharm <martin@binfalse.de>
2015-10-17 10:38:42 +0200

    add checks for URLs

--- src/test/java/de/binfalse/rdf2dot/ConverterTest.java
+++ src/test/java/de/binfalse/rdf2dot/ConverterTest.java
@@ -25,6 +25,8 @@ public class ConverterTest
 	Pattern provActivity = Pattern.compile ("\\[ (shape=\"polygon\"\\s*|sides=\"4\"\\s*|color=\"#000000\"\\s*|label=\"[^\"]\"\\s*|style=\"filled\"\\s*|fillcolor=\"#9FB1FC\"\\s*){6}\\];");
 	Pattern provAgent = Pattern.compile ("\\[ (shape=\"house\"\\s*|color=\"#000000\"\\s*|label=\"[^\"]\"\\s*|style=\"filled\"\\s*|fillcolor=\"#FDB266\"\\s*){5}\\];");
 	Pattern provEntity = Pattern.compile ("\\[ (shape=\"ellipse\"\\s*|color=\"#000000\"\\s*|label=\"[^\"]\"\\s*|style=\"filled\"\\s*|fillcolor=\"#FFFC87\"\\s*){5}\\];");
+	Pattern urlNode = Pattern.compile ("\"http[^\"]+\" \\[ (shape=\"ellipse\"\\s*|color=\"#3C61DD\"\\s*|label=\"http://[^\"]+\"\\s*|style=\"filled\"\\s*|fillcolor=\"#DCE4FF\"\\s*){5}\\];");
+	
 	Pattern provAssociatedWith = Pattern.compile ("\"[^\"]\" -> \"[^\"]\" \\[ label=\"prov:associatedWith\" \\];");
 	Pattern provUsed = Pattern.compile ("\"[^\"]\" -> \"[^\"]\" \\[ label=\"prov:used\" \\];");
 	
@@ -62,7 +64,7 @@ public class ConverterTest
 	 * Simple test.
 	 */
 	@Test
-	public void simpleTest2 ()
+	public void simpleProvTest ()
 	{
 		try
 		{
@@ -73,7 +75,7 @@ public class ConverterTest
 			System.out.println (contents);
 			
 			assertTrue ("expected to obtain a LR graph", contents.contains ("rankdir=\"LR\""));
-			assertEquals ("expected to receive two ordinary connections and three attribute connections", 5, findPattern (dotConnection, contents));
+			assertEquals ("expected to receive three ordinary connections and three attribute connections", 6, findPattern (dotConnection, contents));
 			
 			assertEquals ("expected to obtain a prov:associatedWith connection", 1, findPattern (provAssociatedWith, contents));
 			assertEquals ("expected to obtain a prov:used connection", 1, findPattern (provUsed, contents));
@@ -81,6 +83,8 @@ public class ConverterTest
 			assertEquals ("expected exactly 1 prov entity", 1, findPattern (provEntity, contents));
 			assertEquals ("expected exactly 1 prov agent", 1, findPattern (provAgent, contents));
 			assertEquals ("expected exactly 1 prov activity", 1, findPattern (provActivity, contents));
+			
+			assertEquals ("expected exactly 1 url", 1, findPattern (urlNode, contents));
 			
 			assertFalse ("didn't expect a file name if speaking about internal stuff", contents.contains (new File ("test/simplegraph-2.ttl").getAbsoluteFile ().getParentFile ().getParentFile ().getAbsolutePath ()));
 			assertFalse ("didn't expect a file name if speaking about internal stuff", contents.contains ("simplegraph"));
